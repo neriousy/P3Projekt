d8ed1ef285a0740b87550dd9eb8ada695302b39a
neriousy <filipori7@interia.pl>
2022-06-04 21:17:21 +0200

    Adding packages

--- obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Dziennik_Szkolny {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Dziennik-Szkolny;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Dziennik-Szkolny;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
