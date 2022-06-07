3d7936ed843c693422b8b0ec9ffb8cf6b3d2624d
xNeriousy <filipori7@interia.pl>
2022-06-07 18:35:22 +0200

    folder fix

---
+++ Klient/Views/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using MaterialDesignThemes.Wpf;
+namespace Dziennik_Szkolny
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        public bool darkThemeTurnedOn { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+        public void exitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void changeTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+            if (theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                darkThemeTurnedOn = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                darkThemeTurnedOn = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+    }
+}
