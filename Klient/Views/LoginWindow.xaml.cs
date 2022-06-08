01de50cef35d70439dc5ea4309b36ab23b7821d3
Bartosz Konkel <308177@o365.stud.umk.pl>
2022-06-08 14:20:09 +0200

    sync

--- Klient/Views/LoginWindow.xaml.cs
+++ Klient/Views/LoginWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using MaterialDesignThemes.Wpf;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 namespace Dziennik_Szkolny
 {
     public partial class LoginWindow : Window
@@ -46,9 +49,43 @@ namespace Dziennik_Szkolny
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
-            base.OnMouseLeftButtonDown(e);
-            DragMove();
+            
         }
 
+        async private void loginButton_Click(object sender, RoutedEventArgs e)
+        {            
+        /*    Socket clientSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            EndPoint serverAddress = new IPEndPoint(IPAddress.Loopback, 7777);
+            await Task.Factory.StartNew(() => clientSocket.Connect(serverAddress));
+            NetworkStream strumienPolaczenia = new NetworkStream(clientSocket);
+            StreamWriter strumienZapisu = new StreamWriter(strumienPolaczenia);
+            StreamReader strumienOdczytu = new StreamReader(strumienPolaczenia);
+            (string, string) wiadomoscWys = (userInputtedUsername.Text, userInputtedPassword.Password.ToString());
+            await Task.Factory.StartNew(() => { strumienZapisu.WriteLine(wiadomoscWys); });
+            await Task.Factory.StartNew(strumienZapisu.Flush);
+            (int, char) wiadomoscOdb = await Task<(int, char)>.Factory.StartNew(strumienOdczytu.ReadLine);
+            strumienZapisu.Close();
+            strumienOdczytu.Close();
+            strumienPolaczenia.Close();
+            clientSocket.Close();
+
+            if(wiadomoscOdb.Item1 != -1)
+            {
+                MainWindow main = new MainWindow();
+                App.Current.MainWindow = main;
+                this.Hide();
+                main.Show();
+            }
+            else
+            {
+                string message = "Wprowadzono niepoprawne dane, sprobuj ponownie!";
+                string title = "Blad";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBox.Show(message, title, button);
+            }*/
+        }
     }
 }
